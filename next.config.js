f4a8e9982cdb9c10531afaec82473abe12cb9e75
thekrishna7 <krishnasharmaambah961u@gmail.com>
2026-07-29 11:09:10 +0530

    Clean next.config.js configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
